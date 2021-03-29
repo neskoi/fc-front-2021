784305e687eb7146f3a539fca054289742c02a02
pedromaranini <you@example.com>
2021-03-29 11:45:08 -0300

    feat: iniciado a codificacao da tela de login

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Login from './pages/Login';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Home} />
+            <Route path="/" exact component={Login} />
         </BrowserRouter>
     );
 }
