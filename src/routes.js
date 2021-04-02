1cafdfd1c6103f2dfd9ba4d2929dabc242619efb
neskoi <57025040+neskoi@users.noreply.github.com>
2021-04-02 17:29:59 -0300

    Feat checkout (#14)
    * feat: adição da tela de checkout.
    
    * feat: adição de cores a tela de checkout.
    
    * feat: componente footer adicionado.
    
    * feat: adição do componente Navbar, falta logica.
    
    * feat: adição da tela de orçamento do aluno, componentes adicionados: ProgressBar, OrangeButton.
    
    * feat: tela de checkout com as cores e icones do prototipo de alta.

--- src/routes.js
+++ src/routes.js
@@ -8,22 +8,21 @@ import Login from './pages/Login';
 import SignupPage from './pages/SignupPage/SignupPage'
 import NewStudentPage from './pages/NewStudentPage/NewStudentPage';
 import StudentsPage from './pages/StudentsPage/StudentsPage'
-import Modal from './components/ConfirmationMessage/ConfirmationMessage';
 import GoalPage from './pages/GoalPage/GoalPage';
 import ResponsiblePage from './pages/ResponsiblePage/ResponsiblePage';
-
 function Routes() {
     return (
         <BrowserRouter>
+            <Route path="/" exact component={Login} />
             <Route path="/cadastro" exact component={Register} />
             <Route path="/cadastro/pessoa" exact component={PersonRegister} />
             <Route path="/cadastro/sucesso" exact component={Success} />
-            <Route path="/" exact component={Login} />
             <Route path="/signup" exact component={SignupPage} />
             <Route path="/students" exact component={StudentsPage} />
             <Route path="/newstudent" exact component={NewStudentPage} />
             <Route path="/goal" exact component={GoalPage} />
             <Route path="/responsible" exact component={ResponsiblePage} />
+
         </BrowserRouter>
     );
 }
